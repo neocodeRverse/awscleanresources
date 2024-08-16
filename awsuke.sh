e60fd0acbbe6a6a1acf9422584307b0d1c8d8369
Raghuveer <163999232+neocodeRverse@users.noreply.github.com>
2024-08-16 16:20:23 +0530

    Create awsuke.sh

---
+++ awsuke.sh
@@ -0,0 +1,5 @@
+wget https://github.com/rebuy-de/aws-nuke/releases/download/v2.25.0/aws-nuke-v2.25.0-linux-amd64.tar.gz
+tar -xvf aws-nuke-v2.25.0-linux-amd64.tar.gz
+sudo mv aws-nuke-v2.25.0-linux-amd64 /usr/local/bin/aws-nuke
+sudo chmod +x /usr/local/bin/aws-nuke
+aws-nuke version
